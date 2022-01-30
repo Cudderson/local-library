8a154192188127016884100bab6d4051163edc5f
cudderson <codered1227@gmail.com>
2022-01-30 15:35:53 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const nunjucks = require('nunjucks');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
+
+const mongoDbConnectionString = require('./connect.js');
+
+
+var app = express();
+
+// set up monggose connection
+var mongoose = require('mongoose');
+var mongoDB = mongoDbConnectionString;
+
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+// bind connection to error event to view errors
+db.on('error', console.error.bind(console, "MongoDb connection error"));
+
+// set default express engine and extension
+// It works without this, saving just in case
+// app.engine('html', nunjucks.render);
+// app.set('view engine', 'html');
+
+// configure nunjucks
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); // add catlog routes to middleware chain
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  // This redirects to the specified page, by default sending HTTP status code "302 Found".
+  res.redirect('/catalog');
+});
+
+
+module.exports = router;
