14cf3bbcb5a476d5b5948a441dd58f503cab3e6b
cudderson <codered1227@gmail.com>
2022-01-30 15:47:32 -0600

    fix node modules inclusion

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 
-const mongoDbConnectionString = require('./connect.js');
+const mongoDbConnectionString = require('./connect.js'); // imported for privacy
 
 
 var app = express();
